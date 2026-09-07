47a3c3fdb93fc19fdfaeb83a8049fbfe0838ce4b
shubh912 <57699783+shubh912@users.noreply.github.com>
2026-09-07 19:02:54 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
+
 import App from './App.tsx'
+import Review from './Review.tsx'
+
+const path = window.location.pathname
+
+const Page = path === '/review' ? Review : App
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Page />
   </StrictMode>,
 )
